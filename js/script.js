3f2e326169aa7e431078a12312df1efffa0d7020
md-sabbir51 <developersabbir001@gmail.com>
2023-12-21 02:42:16 -0800

    New code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Header-slider start
 $(".header-slider").owlCarousel({
     // autoplay: true,
     // loop: true,
@@ -13,7 +14,7 @@ $(".header-slider").owlCarousel({
         },
       },
     });
-
+//Header-slider end
 // counter start
 $(".counter").counterUp({
   delay: 10,
@@ -24,3 +25,42 @@ $(".counter").counterUp({
   },
 });
 // counter end
+// Service Animation-click start
+const iconAnimation = document.querySelector('.iconAnimation');
+const icon = document.querySelector('.icon')
+
+iconAnimation.addEventListener('click', function(){
+  icon.classList.toggle('animate')
+});
+
+const iconAnimation2 = document.querySelector('.iconAnimation2');
+const icon2 = document.querySelector('.icon2')
+
+iconAnimation2.addEventListener('click', function(){
+  icon2.classList.toggle('animate2')
+});
+
+const iconAnimation3 = document.querySelector('.iconAnimation3');
+const icon3 = document.querySelector('.icon3')
+
+iconAnimation3.addEventListener('click', function(){
+  icon3.classList.toggle('animate3')
+});
+// Service Animation-click end
+//Header-slider start
+$(".jon-slider").owlCarousel({
+  autoplay: true,
+  loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+//Header-slider end
