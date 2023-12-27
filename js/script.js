debcb6cf1b895a2658cdb2b48b42b0d1953a00b5
md-sabbir51 <developersabbir001@gmail.com>
2023-12-27 10:27:08 -0800

    new resposive

--- js/script.js
+++ js/script.js
@@ -49,8 +49,8 @@ iconAnimation3.addEventListener('click', function(){
 // Service Animation-click end
 //Header-slider start
 $(".jon-slider").owlCarousel({
-  autoplay: true,
-  loop: true,
+  // autoplay: true,
+  // loop: true,
     responsive: {
       0: {
         items: 1,
@@ -64,3 +64,17 @@ $(".jon-slider").owlCarousel({
     },
   });
 //Header-slider end
+
+// Mobile menu
+const hamburger = document.querySelector('.hamburger');
+const bar1 = document.querySelector('.bar1')
+const bar2 = document.querySelector('.bar2')
+const bar3 = document.querySelector('.bar3')
+const mobilenav = document.querySelector('.mobilenav');
+
+hamburger.addEventListener('click', function(){
+    bar1.classList.toggle('animateBar1')
+    bar2.classList.toggle('animateBar2')
+    bar3.classList.toggle('animateBar3')
+    mobilenav.classList.toggle('openDarwer')
+})
