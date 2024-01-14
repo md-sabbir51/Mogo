aeb89f47704b09e6b57bd6557d2203eff45ec3b7
md-sabbir51 <developersabbir001@gmail.com>
2024-01-14 01:06:41 -0800

    mogo-end

--- js/script.js
+++ js/script.js
@@ -26,26 +26,26 @@ $(".counter").counterUp({
 });
 // counter end
 // Service Animation-click start
-const iconAnimation = document.querySelector('.iconAnimation');
-const icon = document.querySelector('.icon')
+// const iconAnimation = document.querySelector('.iconAnimation');
+// const icon = document.querySelector('.icon')
 
-iconAnimation.addEventListener('click', function(){
-  icon.classList.toggle('animate')
-});
+// iconAnimation.addEventListener('click', function(){
+//   icon.classList.toggle('animate')
+// });
 
-const iconAnimation2 = document.querySelector('.iconAnimation2');
-const icon2 = document.querySelector('.icon2')
+// const iconAnimation2 = document.querySelector('.iconAnimation2');
+// const icon2 = document.querySelector('.icon2')
 
-iconAnimation2.addEventListener('click', function(){
-  icon2.classList.toggle('animate2')
-});
+// iconAnimation2.addEventListener('click', function(){
+//   icon2.classList.toggle('animate2')
+// });
 
-const iconAnimation3 = document.querySelector('.iconAnimation3');
-const icon3 = document.querySelector('.icon3')
+// const iconAnimation3 = document.querySelector('.iconAnimation3');
+// const icon3 = document.querySelector('.icon3')
 
-iconAnimation3.addEventListener('click', function(){
-  icon3.classList.toggle('animate3')
-});
+// iconAnimation3.addEventListener('click', function(){
+//   icon3.classList.toggle('animate3')
+// });
 // Service Animation-click end
 //Header-slider start
 $(".jon-slider").owlCarousel({
